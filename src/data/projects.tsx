b40614827ca65064c14da5be644247ddbd8eea8a
Brad Teague <b_rad88@live.com>
2022-07-07 06:36:10 +0930

    add project information

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -2,11 +2,14 @@ import Satisfactory from "pages/projects/individual-pages/Satisfactory";
 import Photography from "pages/projects/Photography";
 import satisfactoryImg from "assets/satisfactory.webp";
 import photographyImg from "assets/bg.jpg";
+import pwgenImg from "assets/pwgen.gif";
+import quizzlesImg from "assets/quizzles.gif";
 export interface Project {
   id: string;
   path: string;
   title: string;
   repos: string[];
+  deployed?: string;
   desc: string;
   complete: boolean;
   page: React.ReactNode;
@@ -22,7 +25,7 @@ const projects: Project[] = [
       "https://github.com/bradbrad88/satisfactory-client",
       "https://github.com/bradbrad88/satisfactory-server",
     ],
-    desc: "PERN stack project",
+    desc: "PERN stack project.",
     complete: false,
     page: <Satisfactory />,
     image: satisfactoryImg,
@@ -31,11 +34,43 @@ const projects: Project[] = [
     id: "photography",
     path: "react-photography",
     title: "React Photography",
-    repos: [],
-    desc: "",
+    repos: [
+      "https://github.com/bradbrad88/photography-client",
+      "https://github.com/bradbrad88/photography-server",
+    ],
+    desc: "PERN stack project featuring account management.",
     complete: false,
     page: <Photography />,
     image: photographyImg,
+  },
+  {
+    id: "pwgen",
+    page: (
+      <div>
+        <h1>PW Generator</h1>
+      </div>
+    ),
+    title: "Password Generator",
+    complete: true,
+    desc: "Vanialla HTML, CSS and JS. A simple application that generates random passwords with the ability to set a few parameters.",
+    image: pwgenImg,
+    path: "pw-generator",
+    repos: ["https://github.com/bradbrad88/password-generator"],
+  },
+  {
+    id: "quizzles",
+    page: (
+      <div>
+        <h1>Quizzles</h1>
+      </div>
+    ),
+    title: "Quizzles",
+    complete: true,
+    desc: "Vanialla HTML, CSS and JS. Renders components dynamically to produce a multiple-choice style game.",
+    image: quizzlesImg,
+    path: "quizzles",
+    repos: ["https://github.com/bradbrad88/code-quiz"],
+    deployed: "https://bradbrad88.github.io/code-quiz/",
   },
 ];
 
