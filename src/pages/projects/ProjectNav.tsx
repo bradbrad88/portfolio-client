b5aecbb67fff3a36af5b40b4785c3452e775f80b
Brad Teague <b_rad88@live.com>
2022-07-08 14:19:57 +0930

    responsive menu icon for project navigation

--- src/pages/projects/ProjectNav.tsx
+++ src/pages/projects/ProjectNav.tsx
@@ -1,20 +1,44 @@
+import { useState, useEffect, MouseEventHandler } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import ButtonList from "components/widgets/ButtonList";
 import { Project } from "data/projects";
+import { menuIcon } from "assets/svgIcons";
 
 const ProjectNav = ({ projects }: { projects: Project[] }) => {
+  const [open, setOpen] = useState(false);
   const nav = useNavigate();
+  useEffect(() => {
+    const handler = () => {
+      setOpen(false);
+    };
+    window.addEventListener("click", handler);
+    window.addEventListener("resize", handler);
+    return () => {
+      window.removeEventListener("click", handler);
+      window.removeEventListener("resize", handler);
+    };
+  }, []);
   const onClick = (project: any) => {
-    console.log(project);
     nav(project.path);
+  };
+  const onMenu: MouseEventHandler<HTMLDivElement> = e => {
+    e.stopPropagation();
+    setOpen(!open);
   };
   const mapId = projects.map(project => ({ ...project, id: project.path }));
   return (
-    <nav>
-      <Link to={"/projects"}>
-        <h2>Projects</h2>
-      </Link>
-      <ButtonList className={"link-hover"} list={mapId} onClick={onClick} />
+    <nav className="project-nav">
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <div className="nav-menu" onClick={onMenu}>
+          {menuIcon(40)}
+        </div>
+      </div>
+      <div className={`display-menu ${open && "active"}`}>
+        <Link to={"/projects"}>
+          <h2>Projects</h2>
+        </Link>
+        <ButtonList className={"link-hover"} list={mapId} onClick={onClick} />
+      </div>
     </nav>
   );
 };
