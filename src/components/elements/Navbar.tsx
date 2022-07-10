d5eb63786981b2a1931b335d31e2662aceb210bf
Brad Teague <b_rad88@live.com>
2022-07-10 22:45:29 +0930

    Navbar contact link fix

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,13 +1,11 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classnames from "classnames";
-import resume from "assets/Resume.pdf";
 import useScrollDirection from "hooks/useScrollDirection";
+import resume from "assets/Resume.pdf";
 import "stylesheets/Navbar.scss";
 
 const NavBar = () => {
   const { up } = useScrollDirection();
-  const location = useLocation();
-  console.log(location.pathname.split("/")[1]);
   const classes = classnames("main", { display: up });
   return (
     <nav className={classes}>
@@ -27,7 +25,7 @@ const NavBar = () => {
           </a>
         </li>
         <li className="link-hover">
-          <Link to={"/#contact"}>Contact</Link>
+          <Link to={"/contact"}>Contact</Link>
         </li>
       </ul>
     </nav>
