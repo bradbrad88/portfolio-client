2c8e180e237b5cccc4522dd044eac1258e22359d
Brad Teague <b_rad88@live.com>
2022-07-30 14:53:01 +0930

    useScrollDirection handle overscroll

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -7,8 +7,13 @@ const useScrollDirection = () => {
   const onscroll: EventListenerOrEventListenerObject = useCallback(
     (e: Event) => {
       const target = e.target as Document;
-      const scrollTop = target.scrollingElement?.scrollTop;
-      if (!scrollTop) return;
+      const scrollTop = target.scrollingElement?.scrollTop || 0;
+      const scrollHeight = target.scrollingElement?.scrollHeight || 0;
+      const clientHeight = target.scrollingElement?.clientHeight || 0;
+
+      if (!scrollTop || scrollTop < 0) return;
+      if (scrollHeight < scrollTop + clientHeight) return;
+
       const scrolledDown = scrollTop > scrollRef.current;
       const scrolledUp = scrollTop < scrollRef.current;
       if (scrolledDown && !down) {
