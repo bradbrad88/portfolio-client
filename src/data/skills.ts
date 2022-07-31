3167d0470d44fa1e4e800026ad2224f1598408a7
Brad Teague <b_rad88@live.com>
2022-07-31 12:37:40 +0930

    Update skills data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -44,13 +44,13 @@ const skills: Skill[] = [
         description: "CSS Basics",
         stars: 4,
         comment:
-          "Confident I can achieve nearly any design, given enough time. Good knowledge of selectors, formatting and layouts.",
+          "Confident I can achieve nearly any design, given time. Good knowledge of selectors, formatting and layouts.",
       },
       {
         description: "CSS Layous",
         stars: 4,
         comment:
-          "Comfortable with grid and flex systems. Not to brag but I once centred an element 😜\nTried",
+          "Comfortable with grid and flex systems. Solid understanding of the box model.",
       },
     ],
   },
@@ -133,7 +133,11 @@ const skills: Skill[] = [
         comment:
           "Competent with managing state and a good understanding of renders performed due to state change. Able to memoize data when required. ",
       },
-      { description: "Context", stars: 4, comment: "" },
+      {
+        description: "Context",
+        stars: 4,
+        comment: "Have worked with both class and functional context. ",
+      },
       {
         description: "Routing",
         stars: 3,
@@ -145,6 +149,52 @@ const skills: Skill[] = [
         stars: 3,
         comment:
           "A little rough but making progress with typing in React. Have encountered difficulties with typing contexts where typescript expects the possibility of a null value that will never happen. Have found solutions to all the issues I've encountered but it can be time consuming.",
+      },
+    ],
+  },
+  {
+    skill: "Node",
+    concepts: [
+      {
+        description: "Express",
+        stars: 4,
+        comment:
+          "Comfortable with the core elements of Express. Able to handle errors, route effectively, write and implement middleware and process responses.",
+      },
+      {
+        description: "Libraries",
+        stars: 3,
+        comment:
+          "Some experience with Sequelize, Passport, Bcrypt, Express-Sessions, Jsonwebtoken, AWS-SDK, Multer, Nodemailer, Sharp (image manipulation)",
+      },
+    ],
+  },
+  {
+    skill: "Databases",
+    concepts: [
+      {
+        description: "Relational Databases",
+        stars: 3,
+        comment:
+          "Only rating myself a 3 in terms of confidence here given that I've had a glimpse as to how involved DB architecture and admin can be. I'm fairly confident in building up simple data structures. I've had experience in building programs in MS Access that were used in the business I worked in for a few years. The programs contained data on employees in relation to business compliance, for example, who had signed off on Safe Work Practices, who had been onboarded correctly based on which department they were hired into. I created a number of other small utility applications to replace spreadsheets and make my life easier. One of these addressed stock ordering, helping me to achieve some of the best results in terms of minimising stock on hand while almost never running out of items. Currently using PostgreSQL in several applications in development. Trying a pure SQL approach in one app and using an ORM in another.",
+      },
+      {
+        description: "Indexing",
+        stars: 2,
+        comment:
+          "I understand that there are a number of different ways to index a field to optimise it based on how it is most commonly accessed. I also understand that there is a trade-off in terms of space required and that write times become more expensive. I'm only familiar with a b-tree index at this point however.",
+      },
+      {
+        description: "Sql Language",
+        stars: 3,
+        comment:
+          "Still much to learn but able to do most of the basics in terms of CRUD operations, joins, aggregation and analysis.",
+      },
+      {
+        description: "Redis",
+        stars: 1,
+        comment:
+          "No practical experience with this type of DB yet. Basic understanding of how it is an in-memory db used frequently with caching.",
       },
     ],
   },
