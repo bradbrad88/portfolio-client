bab043f990dc06934e0adfce1cc6a689e16b6960
Brad Teague <b_rad88@live.com>
2022-08-27 11:31:35 +0930

    Navbar submenu styling

--- src/components/elements/Submenu.tsx
+++ src/components/elements/Submenu.tsx
@@ -37,7 +37,7 @@ const Submenu = ({ root, menuItems, expand, setExpand }: Proptypes) => {
   const renderMenuItems = () => {
     return menuItems.map(menuItem => (
       <li
-        className="hover:bg-white hover:bg-opacity-30 block transition-colors ease-in-out px-3 py-2 sm:py-1 m-0 cursor-pointer"
+        className="hover:bg-white hover:bg-opacity-20 block transition-colors ease-in-out px-3 py-2 sm:py-1 m-0 cursor-pointer"
         key={menuItem.id}
         onClick={e => handleNavigation(e, menuItem.path)}
       >
@@ -48,7 +48,7 @@ const Submenu = ({ root, menuItems, expand, setExpand }: Proptypes) => {
 
   return (
     <div
-      className="absolute w-full whitespace-nowrap left-0 bg-sky-800 bottom-0 sm:bottom-auto origin-bottom sm:top-full sm:origin-top my-14 sm:mt-2 rounded-md text-white text-left text-lg max-h-[70vh] overflow-y-auto transition-transform"
+      className="absolute w-full whitespace-nowrap left-0 bg-sky-800 sm:bg-black sm:bg-opacity-90 bottom-0 sm:bottom-auto origin-bottom sm:top-full sm:origin-top my-14 sm:mt-2 rounded-md sm:rounded-sm sm:border-[1px] sm:border-white sm:border-opacity-10 text-white text-left text-lg max-h-[70vh] overflow-y-auto transition-transform sm:shadow-lg sm:shadow-black"
       style={{
         transform: expand ? undefined : "scaleY(0)",
       }}
@@ -56,11 +56,11 @@ const Submenu = ({ root, menuItems, expand, setExpand }: Proptypes) => {
       <ul>
         <li
           onClick={e => handleNavigation(e)}
-          className="hover:bg-white hover:bg-opacity-30 block transition-colors ease-in-out px-3 py-3 sm:py-2 m-0 cursor-pointer"
+          className="hover:bg-white hover:bg-opacity-20 block transition-colors ease-in-out px-3 py-3 sm:py-2 m-0 cursor-pointer"
         >
           All
         </li>
-        <hr />
+        <hr className="opacity-30" />
         {renderMenuItems()}
       </ul>
     </div>
