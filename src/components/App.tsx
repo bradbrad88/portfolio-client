c7bfd947b450370e319c380d3fbdde93a77f6c76
Brad Teague <b_rad88@live.com>
2022-08-31 15:29:55 +0930

    Use Blog and Project index pages and include catch all route to Home

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,12 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "components/elements/Navbar";
 import Footer from "components/elements/Footer";
 import Home from "pages/home/Home";
-import Projects from "pages/projects/Projects";
+import ProjectsIndex from "pages/projects/ProjectsIndex";
 import Skills from "pages/skills/Skills";
+import BlogsIndex from "pages/blogs/BlogIndex";
 import ContactPage from "pages/contact/ContactPage";
 import ScrollTop from "./utils/ScrollTop";
 import FlexWrapper from "components/styling/FlexWrapper";
-// import "stylesheets/Main.scss";
-// import "stylesheets/Effects.scss";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
@@ -25,10 +24,12 @@ const App = () => {
       <FlexWrapper>
         <Navbar />
         <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="projects/*" element={<Projects />} />
+          <Route index element={<Home />} />
+          <Route path="projects/*" element={<ProjectsIndex />} />
           <Route path="skills" element={<Skills />} />
+          <Route path="blogs/*" element={<BlogsIndex />} />
           <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
       </FlexWrapper>
