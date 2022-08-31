0c0a5167cfc6b84bb2fe7529361df3ce77a12085
Brad Teague <b_rad88@live.com>
2022-08-31 15:38:45 +0930

    Allow section component to accept styles

--- src/components/elements/Section.tsx
+++ src/components/elements/Section.tsx
@@ -1,10 +1,15 @@
 interface Proptypes {
   children?: React.ReactNode;
+  styles?: React.CSSProperties;
   top?: number;
 }
 
-const Section = ({ children, top }: Proptypes) => {
-  return <section className="mt-8 mb-3 w-full">{children}</section>;
+const Section = ({ children, styles }: Proptypes) => {
+  return (
+    <section style={styles} className="mt-8 mb-3 w-full">
+      {children}
+    </section>
+  );
 };
 
 export default Section;
