a7b795eb28f4dde6a64ca6763a6ae2aae433f42c
Brad Teague <b_rad88@live.com>
2022-08-31 15:39:49 +0930

    Add Blogs nav item

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -12,13 +12,14 @@ const NavBar = () => {
         (up
           ? "animate-drop-in-below sm:animate-drop-in-above"
           : "animate-drop-out-below sm:animate-drop-out-above") +
-        " fixed w-[90%] sm:w-fit bottom-0 left-1/2 -translate-x-1/2 bg-sky-800 sm:bg-transparent mb-3 sm:bottom-auto sm:right-0 mr-2 rounded-lg z-10"
+        " fixed w-[90%]  bottom-0 left-1/2 -translate-x-1/2 bg-sky-800 sm:bg-transparent sm:w-fit  sm:p-3 sm:rounded-none mb-3 sm:bottom-auto sm:right-0 mr-2 rounded-lg z-10"
       }
     >
       <ul className="relative grid grid-flow-col auto-cols-fr justify-center sm:flex sm:gap-6 font-bold ">
         <NavLi to="/" title="Home" />
         <NavLi to="/projects" title="Projects" submenu={projects} />
         <NavLi to="/skills" title="Skills" />
+        <NavLi to="/blogs" title="Blogs" />
         <NavLi to="/contact" title="Contact" />
       </ul>
     </nav>
