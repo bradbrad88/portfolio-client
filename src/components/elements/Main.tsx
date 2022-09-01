c8d36e0b35615c6c2f010e54d8bfb713b3504852
Brad Teague <b_rad88@live.com>
2022-09-01 10:23:08 +0930

    Margin top on main

--- src/components/elements/Main.tsx
+++ src/components/elements/Main.tsx
@@ -1,6 +1,6 @@
 const Main = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <main className="text-white max-w-[700px] mx-auto sm:mt-16 mb-10 px-5 w-full">
+    <main className="text-white max-w-[700px] mx-auto mt-5 sm:mt-16 mb-10 px-5 w-full">
       {children}
     </main>
   );
