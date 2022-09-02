1ee333e8eb969a5323e36e0f76c38da00c3ec76a
Brad Teague <b_rad88@live.com>
2022-09-02 12:13:22 +0930

    Fix post request double sending and credentials

--- src/components/analysis/Traffic.ts
+++ src/components/analysis/Traffic.ts
@@ -1,22 +1,25 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import useFetch, { Req } from "hooks/useFetch";
 
 const Traffic = () => {
   const location = useLocation();
   const { postRequest } = useFetch();
-  useEffect(() => {
-    const sendAnalytics = () => {
-      const req: Req = {
-        url: "analytics",
-        data: {
-          route: location.pathname,
-        },
-      };
-      postRequest(req);
+
+  const sendAnalytics = useCallback(() => {
+    const req: Req = {
+      url: "analytics",
+      withCredentials: true,
+      data: {
+        route: location.pathname,
+      },
     };
+    postRequest(req);
+  }, [location.pathname]);
+
+  useEffect(() => {
     sendAnalytics();
-  }, [location]);
+  }, [sendAnalytics]);
   return null;
 };
 
